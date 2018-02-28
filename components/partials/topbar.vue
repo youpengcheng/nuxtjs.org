0a9e5d042f5439ad2b5d504c998037f30b31b4c1
Alex Chopin <alexandre.chopin@live.fr>
2018-02-28 20:20:06 +0100

    logo

--- components/partials/topbar.vue
+++ components/partials/topbar.vue
@@ -1,89 +1,35 @@
 <template>
   <nav class="nTopbar">
     <n-container>
+      <nuxt-link to="/" class="nTopbar__Logo">
+        <n-logo/>
+      </nuxt-link>
       <ul class="nTopbar__Menu">
         <li>
-          <el-dropdown>
-            <span class="el-dropdown-link">
-              {{ t('learn') }}
-              <i class="el-icon-arrow-down el-icon--right"></i>
-            </span>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>
-                <nuxt-link to="/guide">{{ t('guide') }}</nuxt-link>
-              </el-dropdown-item>
-              <el-dropdown-item>
-                <nuxt-link to="/api">{{ t('api') }}</nuxt-link>
-              </el-dropdown-item>
-              <el-dropdown-item>
-                <nuxt-link to="/examples">{{ t('examples') }}</nuxt-link>
-              </el-dropdown-item>
-              <el-dropdown-item>
-                <nuxt-link to="/faq">{{ t('faq') }}</nuxt-link>
-              </el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
+          <nuxt-link to="/guide/introduction">Documentation</nuxt-link>
         </li>
         <li>
-          <el-dropdown>
-            <span class="el-dropdown-link">
-              {{ t('resources') }}
-              <i class="el-icon-arrow-down el-icon--right"></i>
-            </span>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>
-                <nuxt-link to="/guide">{{ t('guide') }}</nuxt-link>
-              </el-dropdown-item>
-              <el-dropdown-item>
-                <nuxt-link to="/api">{{ t('api') }}</nuxt-link>
-              </el-dropdown-item>
-              <el-dropdown-item>
-                <nuxt-link to="/examples">{{ t('examples') }}</nuxt-link>
-              </el-dropdown-item>
-              <el-dropdown-item>
-                <nuxt-link to="/faq">{{ t('faq') }}</nuxt-link>
-              </el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
+          <nuxt-link to="/resources/modules">Resources</nuxt-link>
         </li>
         <li>
-          <a href="#">Community</a>
+          <nuxt-link to="/community/suuport-us">Community</nuxt-link>
         </li>
         <li>
-          <el-dropdown>
-            <span class="el-dropdown-link">
-              {{ t('translations') }}
-              <i class="el-icon-arrow-down el-icon--right"></i>
-            </span>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>
-                <nuxt-link to="/guide">{{ t('guide') }}</nuxt-link>
-              </el-dropdown-item>
-              <el-dropdown-item>
-                <nuxt-link to="/api">{{ t('api') }}</nuxt-link>
-              </el-dropdown-item>
-              <el-dropdown-item>
-                <nuxt-link to="/examples">{{ t('examples') }}</nuxt-link>
-              </el-dropdown-item>
-              <el-dropdown-item>
-                <nuxt-link to="/faq">{{ t('faq') }}</nuxt-link>
-              </el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
+          <nuxt-link to="/translations">Translations</nuxt-link>
         </li>
       </ul>
     </n-container>
-    <!-- ElementUI Dropdown -->
-    <!-- ElementUI Autocomplete -->
   </nav>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import nLogo from '~/components/svg/nuxtjs.vue'
 import nContainer from '~/components/globals/container.vue'
 
 export default {
   components: {
+    nLogo,
     nContainer
   },
   computed: {
@@ -98,19 +44,28 @@ export default {
 .nTopbar {
   height: 64px;
   background-color: #fff;
+  &__Logo {
+    display: block;
+    margin-top: 10px;
+    padding: 5px 0;
+    float: left;
+    .NuxtJS {
+      height: 34px;
+    }
+  }
   &__Menu {
     height: 64px;
     float: right;
     list-style: none;
     li {
       float: left;
-      padding-left: 30px;
+      padding-left: 45px;
       padding-top: 23px;
       height: 64px;
-      .el-dropdown, a {
+      a {
         cursor: pointer;
         color: $color_vue_blue;
-        padding: 5px;
+        padding: 5px 0;
         font-size: 15px;
         display: inline-block;
       }
